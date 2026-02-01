ef75571830563cd6d6069c14c7abc17ecb55f93f
genex.mustakim <mustakim.hayder@genexinfosys.com>
2026-02-02 00:59:25 +0600

    chore: passport keys issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configurePassportKeys();
+    }
+
+    /**
+     * Configure Passport encryption keys.
+     */
+    private function configurePassportKeys(): void
+    {
+        $privateKeyPath = env('PASSPORT_PRIVATE_KEY');
+        $publicKeyPath = env('PASSPORT_PUBLIC_KEY');
+
+        if ($privateKeyPath && $publicKeyPath) {
+            // Check if it's a base64 encoded key (for Vapor) or file path
+            if (str_contains($privateKeyPath, 'storage/') || str_contains($privateKeyPath, '/')) {
+                // File path - use default behavior
+                Passport::loadKeysFrom(storage_path());
+            } else {
+                // Base64 encoded keys (for Vapor)
+                Passport::keyPath($privateKeyPath);
+            }
+        }
     }
 }
